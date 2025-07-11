4ed8a7d3980f2595d6b8b16591f7a8a940690c33
Daniel <218683191+cyberops2025@users.noreply.github.com>
2025-07-10 20:01:34 -0700

    Move function into separate file.

---
+++ include/get_iface_index.h
@@ -0,0 +1,7 @@
+#ifndef GET_IFACE_INDEX_H
+#define GET_IFACE_INDEX_H
+
+int get_iface_index(int raw_sock, char* iface_name); 
+
+#endif
+
--- src/arp-request.c
+++ src/arp-request.c
@@ -2,8 +2,8 @@
 // https://www.opensourceforu.com/2015/03/a-guide-to-using-raw-sockets/
 #include "../include/includes.h"
 #include "../include/get_raw_socket.h"
+#include "../include/get_iface_index.h"
 
-int get_iface_index(int raw_sock, char* iface_name);
 unsigned char* get_iface_mac(int raw_sock, char* iface_name);
 void print_mac(unsigned char* iface_mac);
 
@@ -23,22 +23,6 @@ int main(int argc, char* argv[]) {
     close(raw_sock);
 
     return 0;
-
-}
-
-int get_iface_index(int raw_sock, char* iface_name) {
-
-    struct ifreq iface_index;
-
-    memset(&iface_index, 0, sizeof(iface_index));
-    strncpy(iface_index.ifr_name, iface_name, IFNAMSIZ-1);
-
-    if ((ioctl(raw_sock, SIOCGIFINDEX, &iface_index)) < 0) {
-        fprintf(stderr, "ioctl() failed. (%d)\n", errno);
-        exit(1);
-    }
-    
-    return iface_index.ifr_ifindex;
 
 }
 
